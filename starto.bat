edc9137255fbbd798ce41d28b2b84fd383d67042
boncabee <73638429+boncabee@users.noreply.github.com>
2022-09-16 03:13:07 +0700

    -
    -

---
+++ starto.bat
@@ -0,0 +1,9 @@
+@echo off
+
+Start /min C:\"Users\Administrator\AppData\Local\Discord\Update.exe" --processStart Discord.exe
+Start /min C:\Users\Administrator\AppData\Local\WhatsApp\WhatsApp.exe
+Start /min https://www.youtube.com/watch?v=jfKfPfyJRdk
+Start /min https://web.facebook.com/
+Start C:\Users\Administrator\Documents\1Servaild\6TempCleaner\TempCleaner_x64.exe
+
+exit
